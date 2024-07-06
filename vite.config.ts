cc32224d8f091f57ed921d057853d244aa890290
Alessandro-pang <pcy1314@163.com>
2024-07-06 08:47:34 +0800

    chore: 修改打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig((conf)=>{
+  const isProd = conf.mode === 'production'
+  return {
+    plugins: [vue()],
+    base: isProd ? '/leafer-games/' : '/',
+  }
 })
